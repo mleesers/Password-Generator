ddbbe3446a284a03e3291d7c8c9e78d8652b4f04
mleesers <mduron432@gmail.com>
2021-06-23 15:17:54 -0500

    finished generate password function and tweaked other functions to fix overall

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
-  generatePassword();
+  password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -13,28 +14,24 @@ function generatePassword(){
   prompts();
   funArray = [];
   n = 0;
-  password = ""
-  if(wantUpper = true){
-    funArray.push(upper());
-  }else{
-    continue;
-  }
-  if(wantLower == true){
-    funArray.push(lower());
-  }else{
-    continue;
-  }
-  if(wantSpecial == true){
-    funArray.push(lower());
-  }else{
-    continue;
-  }
+  password = "";
 
+  console.log(funArray)
   while(n != charLength){
-    funArray[Math.floor(Math.random() * funArray.length)];
-    password.concat(character);
+    if(wantUpper == true){
+      funArray.push(upper());
+    }
+    if(wantLower == true){
+      funArray.push(lower());
+    }
+    if(wantSpecial == true){
+      funArray.push(special());
+    }
+    character = funArray[Math.floor(Math.random() * funArray.length)];
+    password += character;
     n += 1;
   }
+  
   return password;
 }
 
@@ -57,26 +54,24 @@ function lower(){
 }
 
 function special(){
-  
-
+  character = "";
+  list = "!@#$%^&*()_-+=:;?/><,.~`{}[]|";
+  for (i=0;i<1;i++){
+    character += list.charAt(Math.floor(Math.random() *list.length));
+  }
+  return character;
 }
 function prompts(){
   charLength = window.prompt("How many characters would you like your password?[between 8 and 128 characters]: ");
   if(charLength < 8 || charLength > 128){
     window.alert("please enter a valid option: ")
     prompts()
-  }else{
-    continue;
   }
   wantUpper = window.confirm("Do you want upper case letters?");
   wantLower = window.confirm("Do you want lower cases letters?");
   wantSpecial = window.confirm("Do you want special characters?");  
   return wantUpper, wantLower, wantSpecial,charLength;
 }
-
-
-
-
 
 
 
