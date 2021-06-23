3b12204bd3c6a4a25d9fc5296d24d7ae511dbb65
mleesers <mduron432@gmail.com>
2021-06-23 13:28:53 -0500

    moved folders and added prompty function in js file

---
+++ Develop/script.js
@@ -0,0 +1,32 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword(){
+  prompts();
+}
+
+
+
+function prompts(){
+  charLength = window.prompt("How many characters would you like your password?[between 8 and 128 characters]: ");
+  if(charLength < 8 || charLength > 128){
+    window.alert("please enter a valid option: ")
+    prompts()
+  }else{
+    continue;
+  }
+  wantUpper = window.confirm("Do you want upper case letters?");
+  wantLower = window.confirm("Do you want lower cases letters?");
+  wantSpecial = window.confirm("Do you want special characters?");
+  
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
